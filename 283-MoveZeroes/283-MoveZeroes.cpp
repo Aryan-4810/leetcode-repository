cce54017685fe6d2a22d89ee5ce0156eaff6db0e
Aryan Bhatt <aryanbhatt0209@gmail.com>
2026-05-03 22:17:42 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.cpp
@@ -0,0 +1,22 @@
+// Last updated: 5/3/2026, 10:17:41 PM
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = -1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+        if (j == -1) {
+            return;
+        }
+        for (int i = j + 1; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
+    }
+};
