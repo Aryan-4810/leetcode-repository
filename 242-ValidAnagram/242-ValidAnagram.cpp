d43cd3f9f55e57f14a0a7518fb1f965cfd188f55
Aryan Bhatt <aryanbhatt0209@gmail.com>
2026-05-03 22:17:43 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.cpp
@@ -0,0 +1,19 @@
+// Last updated: 5/3/2026, 10:17:42 PM
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        unordered_map<char, int> map;
+        for (int i = 0; i < s.length(); i++) {
+            map[s[i]]++;
+            map[t[i]]--;
+        }
+        for (auto& [ch, freq] : map) {
+            if (freq != 0)
+                return false;
+        }
+        return true;
+    }
+};
