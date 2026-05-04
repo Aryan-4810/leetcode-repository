9237806ade6965e714962bd32e2df58281d2fac3
Aryan Bhatt <aryanbhatt0209@gmail.com>
2026-05-04 20:18:19 +0530

    Create 724-FindPivotIndex

---
+++ 724-FindPivotIndex/version/cpp/724-FindPivotIndex_20260504_201818.cpp
@@ -0,0 +1,20 @@
+// Last updated: 5/4/2026, 8:18:18 PM
+1class Solution {
+2public:
+3    int pivotIndex(vector<int>& nums) {
+4        vector<int> lsum(nums.size(), 0);
+5        vector<int> rsum(nums.size(), 0);
+6        for (int i = 1; i < nums.size(); ++i) {
+7            lsum[i] = lsum[i - 1] + nums[i - 1];
+8        }
+9        for (int i = nums.size() - 2; i >= 0; --i) {
+10            rsum[i] = rsum[i + 1] + nums[i + 1];
+11        }
+12        for (int i = 0; i < nums.size(); i++) {
+13            if (lsum[i] == rsum[i]) {
+14                return i;
+15            }
+16        }
+17        return -1;
+18    }
+19};
