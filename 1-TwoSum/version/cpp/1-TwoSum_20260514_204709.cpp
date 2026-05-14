f51087f3895e4473cdaf2b6d23f20a95c1874a12
Aryan Bhatt <aryanbhatt0209@gmail.com>
2026-05-14 20:47:10 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/version/cpp/1-TwoSum_20260514_204709.cpp
@@ -0,0 +1,15 @@
+// Last updated: 5/14/2026, 8:47:09 PM
+1class Solution {
+2public:
+3    vector<int> twoSum(vector<int>& nums, int target) {
+4        unordered_map<int, int> mpp;
+5        for (int i = 0; i < nums.size(); i++) {
+6            int x = target - nums[i];
+7            if (mpp.count(x)) {
+8                return {mpp[x], i};
+9            }
+10            mpp[nums[i]] = i;
+11        }
+12        return {};
+13    }
+14};
