2add1dfe8acf96b79aa11ce891985218b6021f1d
Aryan Bhatt <aryanbhatt0209@gmail.com>
2026-07-15 00:27:34 +0530

    Create 349-IntersectionofTwoArrays

---
+++ 349-IntersectionofTwoArrays/version/cpp/349-IntersectionofTwoArrays_20260715_002732.cpp
@@ -0,0 +1,25 @@
+// Last updated: 7/15/2026, 12:27:32 AM
+1class Solution {
+2public:
+3    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+4        sort(nums1.begin(), nums1.end());
+5        sort(nums2.begin(), nums2.end());
+6        vector<int> result;
+7        int i = 0;
+8        int j = 0;
+9        while (i < nums1.size() && j < nums2.size()) {
+10            if (nums1[i] < nums2[j]) {
+11                i++;
+12            } else if (nums2[j] < nums1[i]) {
+13                j++;
+14            } else {
+15                if (result.empty() || result.back() != nums1[i]) {
+16                    result.push_back(nums1[i]);
+17                }
+18                i++;
+19                j++;
+20            }
+21        }
+22        return result;
+23    }
+24};
