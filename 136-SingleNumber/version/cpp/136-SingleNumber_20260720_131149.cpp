dfd9866d96caed00623cdab218f0b9d3f1c9dcb5
Aryan Bhatt <aryanbhatt0209@gmail.com>
2026-07-20 13:11:50 +0530

    Create 136-SingleNumber

---
+++ 136-SingleNumber/version/cpp/136-SingleNumber_20260720_131149.cpp
@@ -0,0 +1,16 @@
+// Last updated: 7/20/2026, 1:11:49 PM
+1class Solution {
+2public:
+3    int singleNumber(vector<int>& nums) {
+4        unordered_map<int, int> freq;
+5        for (int num : nums) {
+6            freq[num]++;
+7        }
+8        for (int num : nums) {
+9            if (freq[num] == 1) {
+10                return num;
+11            }
+12        }
+13        return -1;
+14    }
+15};
