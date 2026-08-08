1f93f99c70926e5e98b591fb2097e4a2d772eaa2
Aryan Bhatt <aryanbhatt0209@gmail.com>
2026-08-08 22:52:15 +0530

    Create 88-MergeSortedArray

---
+++ 88-MergeSortedArray/version/cpp/88-MergeSortedArray_20260808_225214.cpp
@@ -0,0 +1,22 @@
+// Last updated: 8/8/2026, 10:52:14 PM
+1class Solution {
+2public:
+3    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+4        int left = m - 1;
+5        int right = 0;
+6        while (left >= 0 && right < nums2.size()) {
+7            if (nums1[left] > nums2[right]) {
+8                swap(nums1[left], nums2[right]);
+9                left--;
+10                right++;
+11            } else {
+12                break;
+13            }
+14        }
+15        sort(nums1.begin(), nums1.begin() + m);
+16        sort(nums2.begin(), nums2.end());
+17        for (int i = 0; i < n; i++) {
+18            nums1[m + i] = nums2[i];
+19        }
+20    }
+21};
