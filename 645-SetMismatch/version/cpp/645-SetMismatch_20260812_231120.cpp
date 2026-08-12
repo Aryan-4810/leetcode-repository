5396ad7d6a78eb9a3c7ab593226072530f353b8c
Aryan Bhatt <aryanbhatt0209@gmail.com>
2026-08-12 23:11:20 +0530

    Create 645-SetMismatch

---
+++ 645-SetMismatch/version/cpp/645-SetMismatch_20260812_231120.cpp
@@ -0,0 +1,24 @@
+// Last updated: 8/12/2026, 11:11:20 PM
+1class Solution {
+2public:
+3    vector<int> findErrorNums(vector<int>& nums) {
+4        vector<int> hash(nums.size() + 1, 0);
+5        for (int i = 0; i < nums.size(); i++) {
+6            hash[nums[i]]++;
+7        }
+8        int repeating = -1;
+9        int missing = -1;
+10        for (int i = 1; i <= nums.size(); i++) {
+11            if (hash[i] == 2) {
+12                repeating = i;
+13            }
+14            else if (hash[i] == 0) {
+15                missing = i;
+16            }
+17            if (repeating != -1 && missing != -1) {
+18                break;
+19            }
+20        }
+21        return {repeating, missing};
+22    }
+23};
