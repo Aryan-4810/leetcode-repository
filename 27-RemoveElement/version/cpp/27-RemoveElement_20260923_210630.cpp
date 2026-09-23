ce4745ffb70b1c7dde683df7bc64e99dd9087168
Aryan Bhatt <aryanbhatt0209@gmail.com>
2026-09-23 21:06:30 +0530

    Create 27-RemoveElement

---
+++ 27-RemoveElement/version/cpp/27-RemoveElement_20260923_210630.cpp
@@ -0,0 +1,14 @@
+// Last updated: 9/23/2026, 9:06:30 PM
+1class Solution {
+2public:
+3    int removeElement(vector<int>& nums, int val) {
+4        int i = 0;
+5        for (int j = 0; j < nums.size(); j++) {
+6            if (nums[j] != val) {
+7                nums[i] = nums[j];
+8                i++;
+9            }
+10        }
+11        return i;
+12    }
+13};
